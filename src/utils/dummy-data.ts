9d22badd241cb075eed64279df286c336e9636c0
manot40 <kevinsandiho290100@gmail.com>
2023-02-18 17:45:20 +0700

    fix: add dummy-content fallback to gist

--- src/utils/dummy-data.ts
+++ src/utils/dummy-data.ts
@@ -1,23 +1,36 @@
 import path from 'path';
 import fs from 'fs/promises';
 
-import { deepFreeze } from '@/utils';
+import { deepFreeze, fetcher } from '@/utils';
 
 let dummyData: DummyData;
 
 export const getDummyData = async <T extends keyof DummyData>(key: T) => {
   if (!dummyData) {
-    const baseDir = path.join(process.cwd().replace(/(\.next).*/, ''), '/dummy-data');
+    const getGist = (hash: string) => `https://gist.githubusercontent.com/manot40/${hash}/raw`;
 
-    const productsPath = path.join(baseDir, 'products.json');
-    const cartsPath = path.join(baseDir, 'carts.json');
-    const usersPath = path.join(baseDir, 'users.json');
+    let results: [Product[], Cart[], User[]];
 
-    const [products, cart, users] = (await Promise.all([
-      fs.readFile(productsPath, 'utf-8'),
-      fs.readFile(cartsPath, 'utf-8'),
-      fs.readFile(usersPath, 'utf-8'),
-    ]).then((res) => res.map((p) => JSON.parse(p)))) as [Product[], Cart[], User[]];
+    try {
+      const baseDir = path.join(process.cwd().replace(/(\.next).*/, ''), '/dummy-data');
+      const productsPath = path.join(baseDir, 'produdcts.json');
+      const cartsPath = path.join(baseDir, 'cartds.json');
+      const usersPath = path.join(baseDir, 'usedrs.json');
+
+      results = (await Promise.all([
+        fs.readFile(productsPath, 'utf-8'),
+        fs.readFile(cartsPath, 'utf-8'),
+        fs.readFile(usersPath, 'utf-8'),
+      ]).then((res) => res.map((p) => JSON.parse(p)))) as [Product[], Cart[], User[]];
+    } catch (error) {
+      results = await Promise.all([
+        fetcher.get<Product[]>(getGist('f39a2f3c2e67cc19b5ac70fbb9df0a7a')),
+        fetcher.get<Cart[]>(getGist('d381c4d53d07cb03f322182d5fb733f8')),
+        fetcher.get<User[]>(getGist('f3323b6b71f94cbb750ca604643a7923')),
+      ]);
+    }
+
+    const [products, cart, users] = results;
 
     const categories = products.reduce((acc, p) => {
       if (!acc.includes(p.category)) acc.push(p.category);
