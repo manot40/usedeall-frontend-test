c74534a392cb17854df60431ff3903f8192ac319
manot40 <kevinsandiho290100@gmail.com>
2023-02-19 14:55:43 +0700

    fix: add dashboard loading overlay

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,29 @@ import Head from 'next/head';
 import { fetcher } from '@/utils';
 
 import PieChart from '@/components/Chart';
-import { Card, Flex, Grid, Title } from '@mantine/core';
+import { Card, Flex, Grid, LoadingOverlay, Title } from '@mantine/core';
 
 export default function Dashboard() {
-  const { data: res, error } = useSWR<Res<ProductChartData>>('/api/dashboard/product', fetcher.get);
+  const { data: res } = useSWR<Res<ProductChartData>>('/api/dashboard/product', fetcher.get);
   return (
     <Grid gutter="md">
       <Head>
         <title>Dashboard - Simple E-Commerce</title>
       </Head>
       <Grid.Col span={12} md={6}>
-        <Card shadow="md">
+        <Card shadow="md" mih={360} style={{ position: 'relative' }}>
           <Title align="center" order={3}>
             Product by Brand
           </Title>
-          <Flex p="md">{res && <PieChart data={res.result!.byBrand} />}</Flex>
+          <Flex p="md">{res ? <PieChart data={res.result!.byBrand} /> : <LoadingOverlay visible />}</Flex>
         </Card>
       </Grid.Col>
       <Grid.Col span={12} md={6}>
-        <Card shadow="md">
+        <Card shadow="md" mih={360} style={{ position: 'relative' }}>
           <Title align="center" order={3}>
             Product by Category
           </Title>
-          <Flex p="md">{res && <PieChart data={res.result!.byCategory} />}</Flex>
+          <Flex p="md">{res ? <PieChart data={res.result!.byCategory} /> : <LoadingOverlay visible />}</Flex>
         </Card>
       </Grid.Col>
     </Grid>
